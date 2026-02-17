be275c8b10da32dcff107da6f89afce0b1863179
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-02-17 13:48:00 -0500

    implement loading to await fetch for UrlData.jsx

--- src/data/UrlData.jsx
+++ src/data/UrlData.jsx
@@ -1,25 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export function UrlData({path}){
-    useEffect(()=>{
-         const fetchData = async()=>{
-            try{
-            const response = fetch(`https://devsapihub.com/api-ecommerce/`)
-            .then((data)=>{
-                data.json()
-                console.log(data)
-            }).catch((error)=> console.log(error))
-            
-        }catch(error){
-            console.log(`Error: ${error}`)
-        }
-         }
-        // LLAMAR A LA FUNCION
-        fetchData();
-    }, [])
-    return(
-        <>
-        consultando API...
-    </>
-    );
+export function UrlData({ path }) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`https://devsapihub.com/api-ecommerce/`)
+          if(!response.ok){
+            console.log("Error peticion.")
+          }
+          const data = await response.json()
+          console.log(`Los Datos del Servidor son:`)
+          console.log(data)
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    // LLAMAR A LA FUNCION
+    fetchData();
+  }, []);
+  return <div>{loading && <p>consultando API...</p>}</div>;
 }
